c06267145205ab7c1272d0d77643aed145b3655c
Brent <brent@swifthippo.net>
2017-04-27 14:55:23 -0700

    Add while interrupt.

--- get_data.py
+++ get_data.py
@@ -2,15 +2,21 @@
 
 from gps.gps_class import GPS
 import time
+import sys
 
 def main():
     gps_data = GPS('/dev/ttyS0')
+    print("Press Control+C to stop.")
     gps_data.start()
 
-    while True:
-        print("lat: {}, lon: {}, elevation: {}ft, speed: {}mph".format(
-            gps_data.lat, gps_data.lon, gps_data.altitude, gps_data.mph))
-        time.sleep(1)
+    try:
+        while True:
+            print("lat: {}, lon: {}, elevation: {}ft, speed: {}mph".format(
+                gps_data.lat, gps_data.lon, gps_data.altitude, gps_data.mph))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        gps_data.stop()
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
